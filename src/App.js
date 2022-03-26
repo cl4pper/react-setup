39fc4e2c0f9205055d4ef46c1fd13ed7ecf05388
cl4pper <cl4pper@gmail.com>
2022-03-26 21:03:47 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// STYLE
+import './App.scss';
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1 className="App__title">react-setup</h1>
+        </div>
+    );
+};
+
+export default App;
