e417fe30ef883f879ee3c60c59f02353135c45b4
cl4pper <cl4pper@gmail.com>
2022-05-25 16:30:02 +0100

    feat: set template links at Topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Topbar } from '@containers';
-import { Link } from '@components';
 // STYLE
 import './App.scss';
 
@@ -8,10 +7,6 @@ const App = (): JSX.Element => {
 	return (
 		<div className="App">
 			<Topbar id="app-topbar" />
-			<div className="App__links">
-				<Link id="app" label=".js" href="" size="lg" />
-				<Link id="app" label=".ts" href="https://github.com/cl4pper/react-setup" size="lg" />
-			</div>
 		</div>
 	);
 };
